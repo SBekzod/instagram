e6047b245da3df8b738ae8d8b56c53e1aba3c6f4
Martin <bekzodsanakulov89@gmail.com>
2021-09-23 20:29:32 +0900

    BRR: Initial Setup and project design construct

---
+++ App.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App: () => Node = () => {
+
+  return (
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
