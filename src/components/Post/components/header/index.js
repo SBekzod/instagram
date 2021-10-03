01685132a44c81ed5d4ea0aed0bced8041f09e56
Martin <bekzodsanakulov89@gmail.com>
2021-10-03 21:37:26 +0900

    feat: header container flexbox design

--- src/components/Post/components/header/index.js
+++ src/components/Post/components/header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import ProfilePicture from '../../../ProfilePicture';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Entypo';
 // const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 import styles from './styles';
@@ -9,14 +9,13 @@ import styles from './styles';
 const Header = ({imageUri, name}) => {
   return (
     <View style={styles.container}>
-      <ProfilePicture uri={imageUri} size={40} />
-      <Text style={styles.name}>{name}</Text>
-      <Icon
-        name="instagram"
-        size={30}
-        color="#000"
-        style={{alignSelf: 'center', marginLeft: 20}}
-      />
+      <View style={styles.conLeft}>
+        <ProfilePicture uri={imageUri} size={40} />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View style={styles.conRight}>
+        <Icon style={styles.conRight} name="dots-three-vertical" size={16} />
+      </View>
     </View>
   );
 };
--- src/components/Post/components/header/styles.js
+++ src/components/Post/components/header/styles.js
@@ -3,13 +3,22 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    // alignItems: "center"
+    justifyContent: 'space-between'
+  },
+  conLeft: {
+    flexDirection: 'row',
+  },
+  conRight: {
+    flexDirection: 'row',
+    alignItems: 'center', 
+    marginRight: 10 
   },
   name: {
     alignSelf: 'center',
     fontWeight: 'bold',
     color: '#4f4f4f',
   },
+  
 });
 
 export default styles;
