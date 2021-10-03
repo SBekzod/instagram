ccdb30aacea02a7d939006ae8baead15bbf88e43
martin <martin@comets.kr>
2021-10-03 20:45:33 +0900

    fix: 3steps to work with icons on IOS

--- src/components/Post/components/header/index.js
+++ src/components/Post/components/header/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import ProfilePicture from '../../../ProfilePicture';
+import Icon from 'react-native-vector-icons/FontAwesome';
+// const myIcon = <Icon name="rocket" size={30} color="#900" />;
+
 import styles from './styles';
 
 const Header = ({imageUri, name}) => {
@@ -8,6 +11,12 @@ const Header = ({imageUri, name}) => {
     <View style={styles.container}>
       <ProfilePicture uri={imageUri} size={40} />
       <Text style={styles.name}>{name}</Text>
+      <Icon
+        name="instagram"
+        size={30}
+        color="#000"
+        style={{alignSelf: 'center', marginLeft: 20}}
+      />
     </View>
   );
 };
--- src/components/Post/components/header/styles.js
+++ src/components/Post/components/header/styles.js
@@ -1,15 +1,15 @@
-import { StyleSheet } from "react-native"; 
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        // alignItems: "center"
-    },
-    name: {
-        alignSelf: "center",
-        fontWeight: "bold",
-        color: "#4f4f4f"
-    }
+  container: {
+    flexDirection: 'row',
+    // alignItems: "center"
+  },
+  name: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: '#4f4f4f',
+  },
 });
 
 export default styles;
