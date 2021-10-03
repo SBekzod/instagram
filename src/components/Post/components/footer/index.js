34c3b65518f5742255e3e9ccfa5216d371a309c0
Martin <bekzodsanakulov89@gmail.com>
2021-10-03 23:13:15 +0900

    feat: react hooks for liked state manipulation

--- src/components/Post/components/footer/index.js
+++ src/components/Post/components/footer/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Footer = ({caption, likedCounts, postedAt}) => {
+  const [isLiked, toggleLikeState] = useState(false);
+
   return (
-    <View style={styles.container}>
+    <React.Fragment style={styles.mainContainer}>
+      <View style={styles.groupFooterHeader}>
+        <View style={styles.groupRight}>
+          <Icon name={isLiked ? "heart" : "heart-o"} size={17} color={'#545454'} onPress={() => {toggleLikeState(!isLiked)}} />
+          <Icon name="comment-o" size={17} color={'#545454'} />
+          <Icon name="send-o" size={17} color={'#545454'} />
+        </View>
+        <Icon
+          name="save"
+          size={17}
+          onPress={() => alert('you pressed')}
+          color={'#545454'}
+        />
+      </View>
+
       <Text style={styles.likedCounts}>{likedCounts} Likes</Text>
       <Text style={styles.caption}>{caption}</Text>
       <Text style={styles.postedAt}>{postedAt}</Text>
-    </View>
+    </React.Fragment>
   );
 };
 
--- src/components/Post/components/footer/styles.js
+++ src/components/Post/components/footer/styles.js
@@ -1,18 +1,39 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'column'
+  },
+  groupFooterHeader: {
+    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  groupRight: {
+    marginLeft: 10,
+    width: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  groupLeft: {
+    flexDirection: 'row',
+    marginRight: 5
+  },
   container: {
     margin: 5,
   },
   likedCounts: {
     margin: 3,
+    paddingLeft: 7
   },
   caption: {
     margin: 3,
+    paddingLeft: 7,
     fontWeight: 'bold',
   },
   postedAt: {
     margin: 3,
+    paddingLeft: 7,
     color: '#8c8c8c',
   },
 });
